1be6a6afc350aba772cf18004d4cf68e13b3e286
Mayar <mayarmohamedhamed12345@gmail.com>
2024-09-25 19:38:56 +0300

    first commit

---
+++ app/Http/Controllers/Admin/Auth/ProfileController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+
+
+    public function dashboard(){
+        $data=[
+            'title'=>'Dashboard'
+        ];
+        return view('admin.dashboard',$data);
+    }
+
+    public function logout(){
+        auth()->logout();
+        return redirect()->route('login')->with('success', 'You have been successfully logged out');
+    }
+
+}
